800ff738c8436264445193c151ae52cb901e2c4f
skxken <113008730+skxken@users.noreply.github.com>
2022-09-07 15:25:30 +0800

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
+        System.out.println("Hello World!");  
     }
 }
