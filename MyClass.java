75e01e96126cc60a2a4bb7db95a1e0a6029bee35
skxken <113008730+skxken@users.noreply.github.com>
2022-09-07 15:07:34 +0800

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        c = a + b;
+        System.out.println(c);
+    }
+}
